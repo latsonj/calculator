e4f84aa305584cea51f3ad74aa7f0c5b2cd5a81d
Felix S <116848884+latsonj@users.noreply.github.com>
2023-06-24 20:10:33 +1200

    Add displayUpper function

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let operationDisplay = document.querySelector(".operate-text");
+let resultDisplay = document.querySelector(".result-text");
+let equals = document.querySelector("#equals");
+let decimalPoint = document.querySelector("#decimal");
+
+let digits = Array.from(document.querySelectorAll(".digits"));
+let operands = Array.from(document.querySelectorAll(".operands"));
+
 function add(a, b) {
   return a + b;
 }
@@ -31,3 +39,19 @@ function operate() {
 }
 
 console.log(operate());
+
+function displayUpper(event) {
+  let buttonText = event.target.textContent;
+  let checkerArray = operationDisplay.textContent.split(" ");
+
+  if (event.target.classList.contains("digits")) {
+    operationDisplay.textContent += `${buttonText}`;
+    console.log(checkerArray);
+  } else if (((checkerArray.length >= 1 && checkerArray.length < 3) && event.target.classList.contains("operands"))) {
+    operationDisplay.textContent += ` ${buttonText} `;
+    console.log(checkerArray);
+  }
+}
+
+digits.forEach((item) => item.addEventListener("click", displayUpper));
+operands.forEach((item) => item.addEventListener("click", displayUpper));
